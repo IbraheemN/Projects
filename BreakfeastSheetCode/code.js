213c3c963dc8713740464a63f4542d9d5aa3ac9a
Ibraheem Noubani <ibraheem1031@gmail.com>
2020-12-26 10:09:43 -0500

    Removed an unnecessary function

--- BreakfeastSheetCode/code.js
+++ BreakfeastSheetCode/code.js
@@ -412,10 +412,4 @@ function onFormSubmit(e) {
     receiptSheet.getRange('B8').setValue(coffeeFinal);
   } 
   
-  function showSidebar() {
-    var html = HtmlService.createHtmlOutputFromFile('Page')
-        .setTitle('My custom sidebar')
-        .setWidth(300);
-    SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
-        .showSidebar(html);
-  }
+ 
